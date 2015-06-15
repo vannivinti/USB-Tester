cc4cea035c2f408a9606323bd735f1b9280ddf1a
vanni.vinti <vanni.vinti@gmail.com>
2015-06-15 14:40:00 +0200

    prova

--- USB Tester/Form1.Designer.cs
+++ USB Tester/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(462, 300);
+            this.ClientSize = new System.Drawing.Size(551, 332);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
